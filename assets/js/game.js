19d80c49bf7c9d9df9ec9abed4750ec41af37a29
Finn Stitcher <foundfootage@protonmail.com>
2022-05-18 00:34:13 -0700

    win condition is possible

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,8 +3,13 @@
 // WIN - player has defeated or skipped all enemies
 // LOSE - player has run out of health
 
-// iterate over array of enemies to perform multiple fights
+// declare global variables
+// use a for loop to iterate over array of enemies to perform multiple fights
+// before each fight, ask the player to fight or skip
+    // if fight: continue
+    // if skip: subtract 2 dollars, and go to the next iteration of the loop
 // for each enemy, run a loop of attacks
+    // on each iteration:
     // calculate enemy hp and display message
         // if 0, go to the next enemy
             // if there is no next enemy, end the game
@@ -12,6 +17,7 @@
         // if 0, end the game
 
 // let playerName = window.prompt("What is your robot's name?");
+// ^ commented out for easier testing
 let playerName = "Rob";
 let playerHealth = 100;
 let playerAttack = 10;
@@ -25,31 +31,38 @@ let enemyAttack = 12;
 
 let fight = function(enemyName) {
     let promptFight = window.prompt("Your opponent is getting ready. Type 'fight' to enter the ring, or 'skip' to back off.");
-    // quickly make sure the prompt input is valid
+    // make sure the input is valid
     promptFight = promptFight.toLowerCase();
     
     if (promptFight === 'fight') {
+        while (enemyHealth > 0) {
+            // subtract playerAttack from enemyHealth, display message describing what happened
+            enemyHealth -= playerAttack;
+            window.alert(`${playerName} attacked ${enemyName}! ${enemyName} now has ${enemyHealth} hit points remaining!`);      
+            
+            // subtrack enemyAttack from playerHealth, display message describing what happened
+            playerHealth -= enemyAttack;
+            window.alert(`${enemyName} attacked ${playerName}! ${playerName} now has ${playerHealth} hit points remaining!`);
+        };
+        window.alert(`${enemyName} crumpled. Congratulations! You have won this battle!`);
         // player deals damage to enemy
         // alert describing the damage is displayed
-        enemyHealth -= playerAttack;
-        window.alert(`${playerName} attacked ${enemyName}! ${enemyName} now has ${enemyHealth} hit points remaining!`);
+
         // if the enemy has zero health another alert is displayed
-        if (enemyHealth === 0) {
-            window.alert(`${enemyName} crumpled. Congratulations! You have won this battle!`)
-        };
+        // if (enemyHealth === 0) {
+        //     window.alert(`${enemyName} crumpled. Congratulations! You have won this battle!`)
+        // };
 
-        // enemy deals damage to player with the same system
-        playerHealth -= enemyAttack;
-        window.alert(`${enemyName} attacked ${playerName}! ${playerName} now has ${playerHealth} hit points remaining!`);
-        if (playerHealth === 0) {
-            window.alert(`${playerName} crumpled. Woe betide, you have lost this battle.`)
-        };
+        // // enemy deals damage to player with the same system
+        // playerHealth -= enemyAttack;
+        // window.alert(`${enemyName} attacked ${playerName}! ${playerName} now has ${playerHealth} hit points remaining!`);
+        // if (playerHealth === 0) {
+        //     window.alert(`${playerName} crumpled. Woe betide, you have lost this battle.`)
+        // };
     }
-
     else if (promptFight === 'skip') {
         let confirmSkip = window.confirm("Are you sure? If you skip, you'll lose 2 dollars, but you may be better equipped for the next battle.");
 
-        // if yes is selected confirmSkip === true
         if (confirmSkip) {
             playerMoney -= 2;
             window.alert(`You have chosen to skip the fight! After paying the toll, you have ${playerMoney} dollars left.`);
@@ -58,13 +71,14 @@ let fight = function(enemyName) {
             fight();
         };
     }
-
     else {
-        window.alert("You need to choose a valid option. Try again!")
+        window.alert("You need to choose a valid option. Try again!");
         fight();
-    }
+    };
 };
 
 for (let i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i])
+    let pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 };
